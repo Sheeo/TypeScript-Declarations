6f67fd8ecaa8144266061c33bcc921b964d011d3
Michael Sondergaard <ms@sp-photo.dk>
2012-10-04 06:15:48 +0200

    Add some more underscore methods

--- underscore.d.ts
+++ underscore.d.ts
@@ -3,11 +3,15 @@ interface UnderscoreTemplate {
 }
 
 interface UnderscoreStatic {
+    bind(fun, context): void;
+    contains(list, value): bool;
     debounce(func, wait: number, immediate?: bool): (any) => any;
     template(templateString: string, data?: any, settings?: any): UnderscoreTemplate;
     extend(destination, ...sources: { }[]);
     each(list: any[], iterator: (element, index:number, list) => void, context?);
     each(object: {}, iterator: (value, key:string, list) => void, context?);
+    map(list: any[], iterator: (element, index:number, list) => void, context?);
+    map(object: { }, iterator: (value, key: string, list) => void , context? );
 }
 
 declare var _: UnderscoreStatic;
